0d8c8aa80b0a21e3595bfad7eb2368c6e3629cca
SukaMCD <fabian25march@gmail.com>
2025-08-11 09:52:49 +0700

    dashboard

--- Kegiatan-Guru/koneksi.php
+++ Kegiatan-Guru/koneksi.php
@@ -10,6 +10,6 @@ $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pa
 if (!$conn) {
     die("Koneksi gagal: " . pg_last_error());
 } else {
-    echo "Koneksi berhasil";
+    // echo "Koneksi berhasil";
 }
 ?>

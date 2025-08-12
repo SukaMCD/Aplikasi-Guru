5ea8f590c84575200715c3a158ea23562e159756
SukaMCD <fabian25march@gmail.com>
2025-08-12 10:08:44 +0700

    Title

--- Kegiatan-Guru/public/admin/tables.php
+++ Kegiatan-Guru/public/admin/tables.php
@@ -29,7 +29,7 @@ include '../../config/koneksi.php'; // Pastikan koneksi database sudah benar
   <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="../assets/img/favicon.png">
   <title>
-    Kegiatan Guru - User Management
+    User Management - Kegiatan Guru
   </title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
